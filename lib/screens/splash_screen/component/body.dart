b3073a544c92c97b5faa0e4fcb6e45ad496a9ea9
Shubham Agarwal <shubahmagarwal@Shubahms-MacBook-Air.local>
2024-05-09 16:36:15 +0530

    first commit

--- lib/screens/splash_screen/component/body.dart
+++ lib/screens/splash_screen/component/body.dart
@@ -2,52 +2,39 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget{
-
-
-
+class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-@override
+  @override
   void initState() {
-    
     super.initState();
-    Timer(Duration (seconds:1) ,() { 
-      Navigator.pushReplacement(context,MaterialPageRoute(
-        builder:(context)=> start_screen))
+    Timer(Duration(seconds: 1), () {
+      // Navigator.pushReplacement(context,MaterialPageRoute(
+      //   builder:(context)=> start_screen))
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child:  Center(
-          child:  Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-              Container(
-                width: 100,
-                height: 100,
-                
-                child: Image.asset('assets/images/logo.webp'
-                
-                )
-                ),
-                
-             const SizedBox(
-              height: 30,
-             ),
-              
-                
-             
-            ],
-          )),
-      )
-    );
+        body: Container(
+      color: Colors.white,
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              width: 100,
+              height: 100,
+              child: Image.asset('assets/images/logo.webp')),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      )),
+    ));
   }
 }
---
+++ lib/screens/start_screen/start_screen.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:weight_track/size_config/size_config.dart';
+
+class StartScreen extends StatelessWidget {
+  StartScreen({Key? key}) : super(key: key);
+
+  final controller = PageController(
+    viewportFraction: 0.9,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.all(getProportionateScreenWidth(15)),
+        child: Container(
+          padding:
+              EdgeInsets.symmetric(vertical: getProportionateScreenHeight(15)),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(55),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: getProportionateScreenHeight(20),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Image.asset(
+                            'assets/images/logo.webp',
+                            height: getProportionateScreenHeight(85),
+                            width: getProportionateScreenWidth(83),
+                          ),
+                        ),
+                        SizedBox(
+                          width: getProportionateScreenWidth(12),
+                        ),
+                        Text(
+                          'Lossy',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(30),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Weight',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: getProportionateScreenWidth(35)),
+                        ),
+                        SizedBox(
+                          width: getProportionateScreenWidth(8),
+                        ),
+                        Text(
+                          'Loss',
+                          style: TextStyle(
+                              color: ThemeData().primaryColorDark,
+                              fontWeight: FontWeight.bold,
+                              fontSize: getProportionateScreenWidth(30)),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'can be easy',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: getProportionateScreenWidth(30)),
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(28),
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(60),
+                    ),
+                    Text(
+                      'Start to measure your weight every day and analyze your dynamic.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(35),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: getProportionateScreenHeight(30),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(60),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigator.of(context)
+                      //     .pushReplacementNamed(AuthScreen.routeName);
+                    },
+                    child: Text(
+                      'Start',
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: getProportionateScreenWidth(135),
+                        vertical: getProportionateScreenHeight(13),
+                      ),
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(20),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Do you have an account? ',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 30, 158, 83),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
